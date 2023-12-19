f449e1e2b5c4ff3748a4dc27e123aeeced165020
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-12-19 11:51:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const byteSize = (str) => {
   // write your code here
+	let n=str.length,c=0;
+	for(let i=0;i<n;i++){
+		 const charCode = str.charCodeAt(i);
+
+    // Counting bytes based on character code range
+    if (charCode <= 0x7F) {
+      c += 1; // ASCII character
+    } else if (charCode <= 0x7FF) {
+      c += 2; // 2-byte character
+    } else if (charCode <= 0xFFFF) {
+      c += 3; // 3-byte character
+    } else if (charCode <= 0x10FFFF) {
+      c += 4; // 4-byte character (up to maximum Unicode character)
+    }
+	}
+	return c;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
